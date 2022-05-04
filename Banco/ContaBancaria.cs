1c26a72e5b2830ea66f61d8ce0c2fd658fe20a50
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-03 21:47:01 -0300

    Conta Bancaria

---
+++ Banco/ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class ContaBancaria
+    {
+        public string Nome_Cliente;
+        public int Idade_Cliente;
+        public int Numero_Conta;
+        public double Saldo_Conta;
+
+
+        public void DepositoConta(double ValorDepositado)
+        {
+            this.Saldo_Conta = this.Saldo_Conta + ValorDepositado;
+        }
+
+        public void SacaConta(double valorSacar)
+        {
+            this.Saldo_Conta = this.Saldo_Conta - valorSacar;
+        }
+
+        public void VisualizarSaldo()
+        {
+            Console.Write("\n\nCliente:" + this.Nome_Cliente);
+            Console.Write("\n\nIdade:" + this.Idade_Cliente);
+            Console.Write("\n\nNumero da Conta:" + this.Numero_Conta);
+            Console.Write("\n\nSaldo Atual:" + this.Saldo_Conta);
+        }
+    }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c1 = new ContaBancaria();
+            c1.Nome_Cliente = Console.ReadLine();
+            c1.Idade_Cliente = int.Parse(Console.ReadLine());
+            c1.Numero_Conta = int.Parse(Console.ReadLine());
+            c1.Saldo_Conta = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
