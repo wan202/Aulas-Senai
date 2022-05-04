90449084959e4d6fcb836185075df5e01b3dd175
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-04 18:07:44 -0300

    Atividade 03-05

---
+++ ContaBanco/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class ContaBancaria
+    {
+        public string Nome_Cliente;
+        public int Idade_Cliente;
+        public int Numero_Conta;
+        public double Saldo_Conta;
+
+
+        public void DepositoConta(double ValorDepositado)
+        {
+            this.Saldo_Conta = this.Saldo_Conta + ValorDepositado;
+        }
+
+        public void SacaConta(double valorSacar)
+        {
+            this.Saldo_Conta = this.Saldo_Conta - valorSacar;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome_Cliente + "  Sua Idade:  " + Idade_Cliente + "  N. da Conta:  " + Numero_Conta + "  N. da Conta:  " + Saldo_Conta ;
+        }
+    }
+}
---
+++ ContaBanco/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c1 = new ContaBancaria();
+            Console.WriteLine("Digite Seu Nome: ");
+            c1.Nome_Cliente = Console.ReadLine();
+            Console.WriteLine("Digite sua Idade: ");
+            c1.Idade_Cliente = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o numero da sua conta: ");
+            c1.Numero_Conta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite deu Saldo: ");
+            c1.Saldo_Conta = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(c1);
+
+            Console.WriteLine("Digite Valor do Seu Deposito: ");
+            int ValorDepositado = int.Parse(Console.ReadLine());
+            c1.DepositoConta(ValorDepositado);
+
+            Console.WriteLine("Saldo Atualizado: ");
+            Console.WriteLine(c1);
+
+            Console.WriteLine("Digite um valor para Sacar: ");
+            int valorSacar = int.Parse(Console.ReadLine());
+            c1.SacaConta(valorSacar);
+
+            Console.WriteLine("Saldo Atualizado: ");
+            Console.WriteLine(c1);
+        }
+    }
+}
