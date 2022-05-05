ad1776ebcb2cc071de94fb113d129ebb1202715b
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-04 21:55:52 -0300

    Atividade Imcompleta

---
+++ Cauculadora/Calcular.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cauculadora
+{
+    class Calcular
+    {
+        public int Numero1 { get; set; }
+
+        public int Numero2 { get; set; }
+
+        public Calcular(int n1, int n2)
+        {
+            Numero1 = n1;
+            Numero2 = n2;
+        }
+
+        public int Soma()
+        {
+            return Numero1 +Numero2;
+        }
+
+        public int Subtracao()
+        {
+            return Numero1 - Numero2;
+        }
+
+        public int Divisao()
+        {
+            return Numero1 / Numero2;
+        }
+
+        public int Multip()
+        {
+            return Numero1 * Numero2;
+        }
+    }
+}
---
+++ Cauculadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cauculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc Numero1, Numero2;
+            Numero1 = Calc;
+            Numero2 = Calcular;
+
+            Console.WriteLine("Informe o Primeiro Valor: ");
+            Numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o Segundo Valor: ");
+            Numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A soma dos dois Valores e: " + calc.Soma);
+            Console.WriteLine("A Subtracao dos dois Valores e: " + calc.Subtracao);
+            Console.WriteLine("A Divisao dos dois Valores e: " + calc.Divisao);
+            Console.WriteLine("A Multiplicacao dos dois Valores e: " + Multipicar);
+        }
+    }
+}
