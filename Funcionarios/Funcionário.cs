3bcef00e3ca6b7691e9b9213926361c692e5750b
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-11 18:29:44 -0300

    atividade 1 10-05

---
+++ Funcionarios/Funcionário.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_10_05
+{
+    class Funcionário
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Departamento { get; set; }
+        public string Email { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionário()
+        {
+
+        }
+
+        public Funcionário(string nome, int idade, string departamento, string email, double salario)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Departamento = departamento;
+            this.Email = email;
+            this.Salario = salario;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome +
+                "\nIdade: " + Idade +
+                "\nDepartamento: " + Departamento +
+                "\nEmail: " + Email +
+                "\nSalario: " + Salario;
+        }
+
+
+    }
+}
---
+++ Funcionarios/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atividade_10_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char newfunc;
+
+            List<Funcionário> func = new List<Funcionário>();
+            do
+            {
+                Console.WriteLine("Digite o nome de Funcionario");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine("Digite a Idade do Funcionario");
+                int idade = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite Qual Departamento");
+                string departamento = Console.ReadLine();
+
+                Console.WriteLine("Digite o Email do Funcionario");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Digite o Salario a Receber");
+                double salario = double.Parse(Console.ReadLine());
+
+                func.Add(new Funcionário(nome, idade, departamento, email, salario));
+
+                Console.Write("Cadastrar um novo funcionario? (s ou n)");
+                newfunc = char.Parse(Console.ReadLine());
+            }
+            while (newfunc == 's');
+
+            Console.WriteLine("########################################################");
+
+            foreach (Funcionário fnc in func)
+            {
+                Console.WriteLine(fnc);
+            }
+            Console.WriteLine("########################################################");
+
+            Console.WriteLine("Quantidade de Trabalhadores: " + func.Count);
+
+            int contador = 0;
+            if (func.Exists(x => x.Departamento == "desing"))
+            {
+                foreach (Funcionário fnc in func)
+                {
+                    contador++;
+                }
+                if (func.Exists(x => x.Departamento == "rh"))
+                {
+                    contador++;
+                }
+
+                if (func.Exists(x => x.Departamento == "ti"))
+                {
+                    contador++;
+                }
+            }
+        }
+
+    }
+}
