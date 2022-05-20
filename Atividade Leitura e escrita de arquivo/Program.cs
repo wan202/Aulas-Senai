3c158f4333ea421319a6813dc8693270104f179d
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-19 22:47:28 -0300

    Atividade Leitura e escrita de arquivo

---
+++ Atividade Leitura e escrita de arquivo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Atividade_Leitura_e_escrita_de_arquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivoOrigem = @"c:\temp\Produtos.txt";
+            string arquivoDestino = @"c:\temp\copia\CpProduto.txt";
+
+            try
+            {
+                string[] linhas = File.ReadAllLines(arquivoOrigem);
+                using (StreamWriter sw = File.AppendText(arquivoDestino))
+                {
+                    foreach (string linha in linhas)
+                    {
+                        sw.WriteLine(linha.ToUpper());
+                        Console.WriteLine(linha);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("erro: " + e.Message);
+            }
+        }
+    }
+}
