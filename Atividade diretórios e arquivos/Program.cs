b80852c49df1fcfdb205e8447850bd5329e2d42d
WandersoN OliveirA <wanderson202@hotmail.com>
2022-05-20 21:17:36 -0300

    Atividade diretórios e arquivos

---
+++ Atividade diretórios e arquivos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Atividade_diretórios_e_arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            IEnumerable<string> allFilesInAllFolders = Directory.EnumerateFiles(@"c:\WaN202", "*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine("file:");
+            foreach (var file in allFilesInAllFolders)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
